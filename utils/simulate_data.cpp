2530d131060adccf83053be1155f6ce1e7f9351e
ericzhou <zl410702@qq.com>
2024-10-27 15:22:51 +0800

    [仿真类]完成CamBASimData类的书写

--- utils/simulate_data.cpp
+++ utils/simulate_data.cpp
@@ -27,7 +27,51 @@ void CamBASimData::generateData() {
     pt_noise.z() = points_.at(i).z() + gaussRand(0.0, pixel_noise_);
     noise_points_.emplace_back(pt_noise);
   }
-  // 
+  // 标记可用的点,被至少两个相机视角看到的map点
+  Eigen::Matrix3d K;
+  K << focal_length_, 0.0, cx_, 0.0, focal_length_, cy_, 0.0, 0.0, 1.0;
+  for (size_t i = 0; i < pt_nums_; i++) {
+    int num_obs = 0;
+    for (const auto &pose : poses_) {
+      Eigen::Vector3d pc =
+          pose.getOrientation() * points_.at(i) + pose.getPosition();
+      if (pc(2) <= std::numeric_limits<double>::epsilon()) {
+        continue;
+      }
+      pc = pc / pc(2);
+      Eigen::Vector3d z = K * pc;
+      //判断投影点是否在图像范围内,通过这种方式表示是否视野中。
+      if (z(0) >= 0 && z(0) < width_ && z(1) >= 0 && z(1) < height_) {
+        num_obs++;
+      }
+    }
+    if (num_obs >= 2) {
+      valid_points_[i] = true;
+    } else {
+      valid_points_[i] = false;
+    }
+  }
+
+  //记录每个视角看到的mark点
+  for (size_t i = 0; i < poses_.size(); ++i) {
+    for (size_t j = 0; j < pt_nums_; ++j) {
+      Eigen::Vector3d pc =
+          poses_[i].getOrientation() * points_[j] + poses_[i].getPosition();
+      //判断Z是否接近0
+      if (pc(2) <= std::numeric_limits<double>::epsilon())
+        continue;
+      pc = pc / pc(2);
+      Eigen::Vector3d z = K * pc;
+      if (z(0) >= 0 && z(0) < width_ && z(1) >= 0 && z(1) < height_) {
+        marker_id_in_pose_[i].push_back(j);
+        std::pair<double, double> noise_z;
+        //测量=真值+噪音
+        noise_z.first = z(0) + gaussRand(0.0, pixel_noise_);
+        noise_z.second = z(1) + gaussRand(0.0, pixel_noise_);
+        meas_in_pose_[i].push_back(noise_z);
+      }
+    }
+  }
 }
 
 } // namespace utils
--- utils/simulate_data.h
+++ utils/simulate_data.h
@@ -4,11 +4,12 @@
 #pragma once
 #include "pose.hpp"
 #include "random.hpp"
-#include <iostream>
-#include <vector>
 
 #include <Eigen/Core>
 #include <Eigen/Dense>
+#include <iostream>
+#include <list>
+#include <vector>
 
 namespace utils {
 /// @brief 本类BundleAdjust的模拟数据。模型为相机，空间中存在模拟3D点。
@@ -22,18 +23,38 @@ public:
         focal_length_(focal_length), cx_(cx), cy_(cy), width_(width),
         height_(height) {}
   void generateData();
+  ~CamBASimData() {}
+  CamBASimData() = default;
+  //禁止复制
+  CamBASimData(const CamBASimData &) = delete;
+  CamBASimData &operator=(const CamBASimData &) = delete;
 
 private:
+  //随机生成的空间3D点
   std::vector<Eigen::Vector3d> points_;
+  //随机生成的空间3D点增加了一些噪声在每个维度
   std::vector<Eigen::Vector3d> noise_points_;
+  //共视超过2个视角的map点被计为可用点
+  std::vector<bool> valid_points_;
+  //按照一定规律生成的pose
   std::vector<pose::QuanPose<double>> poses_;
+  //每个视角看到的map点的id
+  std::vector<std::list<size_t>> marker_id_in_pose_;
+  // map点的实际测量值，真值+随机高斯噪声
+  std::vector<std::list<std::pair<double, double>>> meas_in_pose_;
+
+  //随机生成的pose个数
   size_t pose_nums_;
+  //空间中随机生成的点的个数
   size_t pt_nums_;
+
   double pixel_noise_;
+  //相机参数
   double focal_length_;
   double cx_;
+  double cy_;
+  //图片的宽高
   double width_;
   double height_;
-  double cy_;
 };
 }; // namespace utils
